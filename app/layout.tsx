93e582a97ea9a31ff3970a6f04ade65fa1486df3
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2024-12-28 21:26:55 +0530

    initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { Bricolage_Grotesque, Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin']
+})
+
+const bricolage = Bricolage_Grotesque({
+  variable: '--font-bricolage',
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: '0xSkip',
+  description: 'We help you skipping habits and routines'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${inter.className} ${bricolage.variable} antialiased dark`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className='font-bricolage'>0xSkip</h1>
+    </div>
+  )
+}
